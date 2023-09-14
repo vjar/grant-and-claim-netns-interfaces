b192057884fda18da5c0d6345a996d9e7561e90b
vjar <30459998+vjar@users.noreply.github.com>
2023-09-14 12:44:20 +0300

    feat: implement grantee pid discovery and opening netns fd

--- src/main.c
+++ src/main.c
@@ -4,6 +4,103 @@
 #include <linux/rtnetlink.h>
 #include <fcntl.h>
 #include <sched.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+int32_t wait_for_child(int32_t *pRv) {
+	int32_t wstatus;
+	waitpid(
+		/* pid: wait on any pid */ -1,
+		/* wstatus */ &wstatus,
+		/* options */ 0
+	);
+	if (pRv != NULL) {
+		*pRv = WEXITSTATUS(wstatus);
+	}
+	if (wstatus) {
+		fprintf(stderr, "wait status unsuccessful, exit code %d\n", WEXITSTATUS(wstatus));
+		return 1;
+	}
+	return 0;
+}
+
+int32_t discover_target_pid(int32_t *pPid, const char *pid_discovery_exec, char *argv[], int32_t *infd, int32_t *outfd) {
+	if (pipe(infd) == -1) {
+		perror("pipe");
+		return 1;
+	}
+
+	if (pipe(outfd) == -1) {
+		perror("pipe");
+		return 1;
+	}
+
+	// infd[0] = readable fd of child's stdin
+	// infd[1] = writable fd of child's stdin
+	// outfd[0] = readable fd of child's stdout
+	// outfd[1] = writable fd of child's stdout
+	if (fork() == 0) {
+		// closes for the unused duplicates created by fork
+		if (close(infd[1])) { perror("close"); exit(1); }
+		if (close(outfd[0])) { perror("close"); exit(1); }
+
+		if (dup2(infd[0], STDIN_FILENO) == -1) { perror("dup2"); exit(1); }
+		if (dup2(outfd[1], STDOUT_FILENO) == -1) { perror("dup2"); exit(1); }
+		// closes for the ones left open after duplicating over the
+		// standard streams
+		if (close(infd[0])) { perror("close"); exit(1); }
+		if (close(outfd[1])) { perror("close"); exit(1); }
+
+		if (execve(pid_discovery_exec,
+			argv,
+			&(char*){ NULL }
+		)) {
+			perror("execve");
+			exit(1);
+		}
+		exit(0);
+	} else {
+		if (close(infd[0])) { perror("close"); exit(1); }
+		if (close(outfd[1])) { perror("close"); exit(1); }
+
+		char buf[16];
+		int64_t nbytes = read(outfd[0], buf, sizeof(buf));
+		if (nbytes == -1) {
+			perror("read");
+			return 1;
+		}
+
+		buf[nbytes] = '\0';
+
+		int32_t pid = atoi(buf);
+		if (pid <= 0) {
+			fprintf(stderr, "child wrote invalid pid \"%s\"\n", buf);
+			return 1;
+		}
+		*pPid = pid;
+		return 0;
+	}
+}
+
+int32_t open_grantee_nsfd(const char *pid_discovery_exec, char *argv[], int32_t *nsfd) {
+	int32_t target_pid;
+	int32_t infd[2];
+	int32_t outfd[2];
+	char path[256];
+	int32_t nbytes;
+
+	if (discover_target_pid(&target_pid, pid_discovery_exec, argv, infd, outfd)) { return 1; }
+
+	nbytes = snprintf(path, sizeof(path), "/proc/%d/ns/net", target_pid);
+	assert(nbytes != -1 && (uint64_t)nbytes < sizeof(path));
+	*nsfd = open(path, O_RDONLY | O_CLOEXEC);
+
+	close(infd[1]);
+	close(outfd[0]);
+	if (wait_for_child(NULL)) { return 1; }
+	return 0;
+}
 
 int32_t nl_set_interface_namespace(const char *ifname, uint32_t nsfd) {
 	struct {
@@ -68,12 +165,12 @@ int32_t nl_set_interface_namespace(const char *ifname, uint32_t nsfd) {
 	return 0;
 }
 
-int32_t main() {
-	int32_t grantee_net = open("/proc/82377/ns/net", O_RDONLY | O_CLOEXEC);
-	if (grantee_net == -1) {
-		perror("open");
-		return 1;
-	}
+int32_t main(int32_t, char *argv[]) {
+	// parse configuration
+	static char *grantee_pid_discovery = "/bin/macvtap-mknod-targetpid-discovery";
+
+	int32_t grantee_net = 0;
+	if (open_grantee_nsfd(grantee_pid_discovery, argv, &grantee_net)) { return 1; }
 
 	int32_t claimee_net = open("/proc/self/ns/net", O_RDONLY | O_CLOEXEC);
 	if (claimee_net == -1) {
